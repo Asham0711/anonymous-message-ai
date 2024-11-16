9e0527ade80b3a00a08b051465932635991eb807
Asham0711 <mdashamimad3246@gmail.com>
2024-11-16 12:30:34 +0530

    Add signup api

---
+++ src/app/api/sign-up/route.ts
@@ -0,0 +1,100 @@
+import connectDB from "@/lib/database";
+import UserModel from "@/models/user.model";
+import bcrypt from 'bcryptjs';
+import { sendVerificationEmail } from "@/helpers/sendVerificationEmail";
+import otpGenerator from 'otp-generator';
+
+export async function POST(request: Request) {
+    await connectDB();
+    try {
+        //extracting data from request body
+        const {username, email, password} = await request.json();
+
+        //finding account if exist and verifed
+        const verifiedUser = await UserModel.findOne({
+            username,
+            isVerified: true,
+        });
+
+        if(verifiedUser){
+            return Response.json(
+                {
+                    success: false,
+                    message: 'Username is already taken',
+                },
+                { status: 400 }
+            );
+        }
+
+        //finding account based on email just to check whether user exist or not
+        const existingUser = await UserModel.findOne({email});
+        //generating 6 digit otp by using otp-generator npm package
+        const verifyCode = otpGenerator.generate(6, { upperCaseAlphabets: false, lowerCaseAlphabets: false, specialChars: false,})
+
+        if(existingUser){
+            if(existingUser.isVerified){
+                return Response.json(
+                    {
+                        success: false,
+                        message: 'User already exists with this email',
+                    },
+                    { status: 400 }
+                );
+            }else{
+                //hashing the password using bcryptjs
+                const hashedPassword = await bcrypt.hash(password,10);
+                existingUser.password = hashedPassword;
+                existingUser.verifyCode = verifyCode;
+                existingUser.verifyCodeExpiry = new Date(Date.now() + 3600000);
+                await existingUser.save();
+            }
+        }else{
+            const hashedPassword = await bcrypt.hash(password,10);
+            //creating expiry date for otp
+            const expiryDate = new Date();
+            expiryDate.setHours(expiryDate.getHours() + 1);
+            const newUser = new UserModel({
+                username,
+                email,
+                password: hashedPassword,
+                verifyCode,
+                verifyCodeExpiry: expiryDate,
+                isVerified: false,
+                isAcceptingMessages: true,
+                messages: [],
+            });
+            await newUser.save();
+        }
+
+        //sending otp for verification purpose
+        const emailResponse = await sendVerificationEmail(email,username,verifyCode);
+        //if there is error while sending mail
+        if(!emailResponse.success){
+            return Response.json(
+                {
+                    success:false,
+                    message: emailResponse.message
+                },
+                {status:401}
+            );
+        }
+
+        return Response.json(
+            {
+                success:false,
+                message:"User registered succesfully... Please verify your account"
+            },
+            {status:200}
+        )
+
+    } catch (error) {
+        console.log("Error while Signing up --> ", error);
+        return Response.json(
+            {
+                success:false,
+                message:"Error while signing up"
+            },
+            {status:500}
+        )
+    }
+}
